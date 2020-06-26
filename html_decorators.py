c862a450a8cded43809f8364f794146b7b34a56a
Luis A. Cruz Cruz <info@meluiscruz.com>
2020-06-26 13:59:28 -0500

    Challenge solved by Luis_Cruz. Thanks a lot!

--- html_decorators.py
+++ html_decorators.py
@@ -1,22 +1,32 @@
 def div(func):
     # You have to code here!
-    pass
+    def wrapper(*arg, **kwargs):
+        answer_1 = f'<div>{func(*arg, **kwargs)}</div>'
+        return answer_1
 
+    return wrapper      
 
 def article(func):
     # You have to code here!
-    pass
+    def wrapper(*arg, **kwargs):
+        answer_2 = f'<article>{func(*arg, **kwargs)}</article>'
+        return answer_2
 
+    return wrapper   
 
 def p(func):
     # You have to code here!
-    pass
+    def wrapper(*arg, **kwargs):
+        answer_3 = f'<p>{func(*arg, **kwargs)}</p>'
+        return answer_3
+
+    return wrapper   
 
 
 # Here you must apply the decorators, uncomment this later
-# @div
-# @article
-# @p
+@div
+@article
+@p
 def saludo(nombre):
     return f'¡Hola {nombre}, ¿Cómo estás?'
 
